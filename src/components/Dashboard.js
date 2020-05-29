35dfce715ffd70a263c8e8d0d8e5e5136a13a4a6
mina.h.moghadam <hm.mina@yahoo.com>
2020-05-29 20:32:00 +0430

    Remove geolocation data and use Tehran lat , lng

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,7 @@ import { DailyWeather } from './weather/DailyWeather';
 import { HourlyWeather } from './weather/HourlyWeather'; 
 
 export default class Dashboard extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             showCurrentWeather: false,
@@ -17,9 +17,9 @@ export default class Dashboard extends Component {
         };
         this.ShowBackground = this.ShowBackground.bind(this);
     }
-    componentDidMount(){
+    componentDidMount() {
         //get lat, long of current location
-        GeoLocationService.GetCurrentLocation()
+        /* GeoLocationService.GetCurrentLocation()
         .then((location) => {
             this.GetCurrentweather(location.lat, location.lng);
             this.GetHourlyWeather(location.lat, location.lng);
@@ -27,53 +27,60 @@ export default class Dashboard extends Component {
         })
         .catch((error) => {
             console.log(error)}
-        );
+		); */
+		
+		let location = {
+			lat: 35.6892, lng: 51.3890
+		}
+		this.GetCurrentweather(location.lat, location.lng);
+		this.GetHourlyWeather(location.lat, location.lng);
+		this.GetDailyWeather(location.lat, location.lng);
     };
 
-    GetCurrentweather(lat, lng){
+    GetCurrentweather(lat, lng) {
         WeatherService.GetWeatherByLocation(lat, lng)
-        .then(response =>{
+        .then(response => {
             this.setState(() => ({ weather: response, showCurrentWeather: true }));
             this.setState({currentId: response.weather[0].id});
         })
     };
-    GetHourlyWeather(lat, lng){
+    GetHourlyWeather(lat, lng) {
         WeatherService.GetHourlyWeather(lat, lng)
-        .then(response =>{
+        .then(response => {
             this.setState(() => ({ hourlyWeather: response, showHourlyWeather: true }));
         })
     };
-    GetDailyWeather(lat, lng){
+    GetDailyWeather(lat, lng) {
         WeatherService.GetDailyWeather(lat, lng)
-        .then(response =>{
+        .then(response => {
             this.setState(() => ({ dailyWeather: response, showDailyWeather: true }));
         })
     };
-    ShowBackground(id){
+    ShowBackground(id) {
         let img_name;
 
-        if(id >= 200 && id < 300){
+        if(id >= 200 && id < 300) {
             img_name = 'thunderstorm.jpg';
         }
-        else if(id >= 300 && id < 500){
+        else if(id >= 300 && id < 500) {
             img_name = 'drizzle.jpg';
         }
-        else if(id >= 500 && id < 600){
+        else if(id >= 500 && id < 600) {
             img_name = 'rain.jpg';
         }
-        else if(id >= 600 && id < 700){
+        else if(id >= 600 && id < 700) {
             img_name = 'snow.jpg';
         }
-        else if(id >= 700 && id < 800){
+        else if(id >= 700 && id < 800) {
             img_name = 'atm.jpg';
         }
-        else if(id === 800){
+        else if(id === 800) {
             img_name = 'clear-sky.jpg';
         }
-        else if(id > 800){
+        else if(id > 800) {
             img_name = 'clouds.jpg';
         }
-        else{
+        else {
             img_name = 'dashboard.jpg';
         }
         return img_name;
@@ -84,7 +91,7 @@ export default class Dashboard extends Component {
             <div className="dashboard" style={{backgroundImage: 'url('+process.env.PUBLIC_URL +'/image/weather/'+ this.ShowBackground(this.state.currentId)+')'}}> 
                 <Header/>
                 <div className="content">
-                    <div className="col-12">
+                    <div className="row flex-row col-12">
                         {this.state.showCurrentWeather && <CurrentWeather currentWeather={this.state.weather} currentDate={this.state.date}/>}
                         {this.state.showHourlyWeather && <HourlyWeather hourlyWeather={this.state.hourlyWeather} />}
                         {this.state.showDailyWeather && <DailyWeather dailyWeather={this.state.dailyWeather} />}
